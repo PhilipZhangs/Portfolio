d4af349c37554333ce2b0e22cfc43ae8124bf07f
PhilipZhangs <zhangsphilip@gmail.com>
2024-08-20 09:12:48 -0700

    first commit

---
+++ src/constants.js
@@ -0,0 +1,25 @@
+export const scaleFactor = 4;
+
+export const dialogueData = {
+    pc: `This is my PC. I work mostly in JavaScript/TypeScript these days. I also have experience working in native mobile with Swift and Kotlin.
+      Here is my <a href="https://github.com/philipzhangs" target="_blank">Github</a>!`,
+
+    "cs-degree": `This is my CS degree. I hung it on the wall because I'm proud of it. I started my undergrad studying Business, but during the COVID-19 pandemic, I discovered a passion for technology and creation, which led me to switch majors.`,
+
+    "sofa-table": `A sofa. I like to relax here and eat food. My favorite meal right now is Big Way Hotpot. Check out the <a href="https://github.com/philipzhangs" target="_blank">mobile app</a> I made for them! `,
+
+    tv: `ToDo`,
+
+    bed: `I'm ready for a nap, check out my <a href="https://www.linkedin.com/in/philipzhangs/" target="_blank">LinkedIn</a>!`,
+
+    "resume": `You found my resume! <p></p>
+    <div style="display: flex; justify-content: center; align-items: center; width: 100%; height: 540px; overflow: hidden;">
+      <img src="philipzhang_cv2024.png" style="max-width: 100%; max-height: 100%; object-fit: contain;">
+    </div>`,
+
+    projects: `My friends and I made this cool <a href="https://sites.google.com/view/cmpt362-cell-identifier-g24/home?authuser=0" target="_blank">study tool</a> for biology students!`,
+
+    library: `ToDo`,
+
+    exit: `Thanks for checking out my portfolio! To exit, just close the tab.`,
+  };
