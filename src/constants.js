e13b0f354f472b0b0e7415bfb7ce49c1b8fd6c70
PhilipZhangs <zhangsphilip@gmail.com>
2024-08-23 00:19:07 -0700

    updated ToDos

--- src/constants.js
+++ src/constants.js
@@ -8,7 +8,7 @@ export const dialogueData = {
 
     "sofa-table": `A sofa. I like to relax here and eat food. My favorite meal right now is Big Way Hotpot. Check out the <a href="https://github.com/PhilipZhangs/BigWayMobileApp" target="_blank">mobile app</a> I made for them! `,
 
-    tv: `ToDo`,
+    tv: `Here's an old TV. TV rhymes with TD. I was an intern at TD where As a QA Analyst Intern, I was responsible for reviewing system requirements to develop and implement effective test cases.`,
 
     bed: `I'm ready for a nap, check out my <a href="https://www.linkedin.com/in/philipzhangs/" target="_blank">LinkedIn</a>!`,
 
@@ -20,7 +20,9 @@ export const dialogueData = {
 
     projects: `My friends and I made this cool <a href="https://sites.google.com/view/cmpt362-cell-identifier-g24/home?authuser=0" target="_blank">study tool</a> for biology students! We developed the mobile app on Android Studio using Kotlin.`,
 
-    library: `ToDo`,
+    library: `Oh wow look at this old book! As an intern for BCL, I have a multi-layered role. I first work in a product team as a QA and work to improve the quality of the software within my team by creating quality and testable code.`,
 
     exit: `Thanks for checking out my portfolio! To exit, just close the tab.`,
+
+    // furniture: `As the Software Engineer in Test Intern, I was entrusted with a wide range of responsibilities, frequently requiring me to adapt and acquire new skills and technologies to meet the evolving needs of the team. This dynamic role allowed me to develop a versatile skill set and respond effectively to the various challenges presented throughout the internship.`
   };
