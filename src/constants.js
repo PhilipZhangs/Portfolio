31b547073fe6d9682f78c08f14e441721403b511
Phil <zhangsphilip@gmail.com>
2024-11-07 14:11:37 -0800

    november update

--- src/constants.js
+++ src/constants.js
@@ -1,16 +1,15 @@
 export const scaleFactor = 4;
 
 export const dialogueData = {
-    pc: `This is my PC. I work mostly in JavaScript/TypeScript these days. I also have experience working in native mobile with Swift and Kotlin.
-      Here is my <a href="https://github.com/philipzhangs" target="_blank">Github</a>!`,
+    pc: `This is my computer. In my personal life, I enjoy gaming with friends. Growing up I loved playing Maplestory but now I mainly play titles from Riot Games with friends.`,
 
-    "cs-degree": `This is my CS degree. I hung it on the wall because I'm proud of it. I started my undergrad studying Business, but during the COVID-19 pandemic, I discovered a passion for technology and creation, which led me to switch majors.`,
+    "cs-degree": `This is my CS degree. I hung it on the wall because I'm proud of it. I started my undergrad studying Business, but during the COVID-19 pandemic, I discovered a passion for technology and here I am now!`,
 
-    "sofa-table": `A sofa. I like to relax here and eat food. My favorite meal right now is Big Way Hotpot. Check out the <a href="https://github.com/PhilipZhangs/BigWayMobileApp" target="_blank">mobile app</a> I made for them! `,
+    "sofa-table": `A sofa. I like to relax here and eat food. I love Big Way Hotpot, so much I even created an app for them. Check out the <a href="https://github.com/PhilipZhangs/BigWayMobileApp" target="_blank">app</a> here!`,
 
-    tv: `Here's an old TV. TV rhymes with TD. I was an intern at TD where As a QA Analyst Intern, I was responsible for reviewing system requirements to develop and implement effective test cases.`,
+    tv: `Here is my <a href="https://github.com/philipzhangs" target="_blank">Github</a>!`,
 
-    bed: `I'm ready for a nap, check out my <a href="https://www.linkedin.com/in/philipzhangs/" target="_blank">LinkedIn</a>!`,
+    bed: `Check out my <a href="https://www.linkedin.com/in/philipzhangs/" target="_blank">LinkedIn</a>!`,
 
     "resume": `You found my <a href="https://github.com/PhilipZhangs/Resume" target="_blank">resume</a>!
     <p></p>
@@ -18,11 +17,11 @@ export const dialogueData = {
       <img src="philipzhang_cv2024.png" style="max-width: 100%; max-height: 100%; object-fit: contain;">
     </div>`,
 
-    projects: `My friends and I made this cool <a href="https://sites.google.com/view/cmpt362-cell-identifier-g24/home?authuser=0" target="_blank">study tool</a> for biology students! We developed the mobile app on Android Studio using Kotlin.`,
+    projects: `My friends and I made this cool <a href="https://sites.google.com/view/cmpt362-cell-identifier-g24/home?authuser=0" target="_blank">study tool</a> for biology students! We developed the mobile app using Android Studio and Kotlin.`,
 
-    library: `Oh wow look at this old book! As an intern for BCL, I have a multi-layered role. I first work in a product team as a QA and work to improve the quality of the software within my team by creating quality and testable code.`,
+    library: `Under construction`,
+
+    furniture: `Under construction`,
 
     exit: `Thanks for checking out my portfolio! To exit, just close the tab.`,
-
-    // furniture: `As the Software Engineer in Test Intern, I was entrusted with a wide range of responsibilities, frequently requiring me to adapt and acquire new skills and technologies to meet the evolving needs of the team. This dynamic role allowed me to develop a versatile skill set and respond effectively to the various challenges presented throughout the internship.`
   };
