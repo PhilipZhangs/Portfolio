0c9fc115f2bfa9d58327c1a727f2baeaf0340c2f
Phil <zhangsphilip@gmail.com>
2025-02-04 01:24:43 -0800

    updated text

--- src/constants.js
+++ src/constants.js
@@ -1,27 +1,41 @@
 export const scaleFactor = 4;
 
 export const dialogueData = {
-    pc: `This is my computer. In my personal life, I enjoy gaming with friends. Growing up I loved playing Maplestory but now I mainly play titles from Riot Games with friends.`,
+    // pc: `This is my computer. In my personal life, I enjoy gaming with friends. Growing up I loved playing Maplestory but now I mainly play titles from Riot Games with friends.`,
+    pc: `in my spare time, i like to unwind by playing video games with friends. i rarely play solo because i only do it to spend time with others. some titles i play are League of Legends, Valorant, Marvel Rivals, Stardew Valley and Minecraft.`,
 
-    "cs-degree": `This is my CS degree. I hung it on the wall because I'm proud of it. I started my undergrad studying Business, but during the COVID-19 pandemic, I discovered a passion for technology and here I am now!`,
+    // "cs-degree": `This is my CS degree. I hung it on the wall because I'm proud of it. I started my undergrad studying Business, but during the COVID-19 pandemic, I discovered a passion for technology and here I am now!`,
+    "cs-degree": `i studied computer science and graduated from sfu. im very proud of this accomplishment because for half of my undergrad, i couldn't figure out what i wanted to do. i started in econ, switched into business and then found myself in comp sci in the midst of covid. fun fact; i had to retake data structures and algorithms twice, its a little embarrassing but humility builds character right?`,
 
-    "sofa-table": `A sofa. I like to relax here and eat food. I love Big Way Hotpot, so much I even created an app for them. Check out the <a href="https://github.com/PhilipZhangs/BigWayMobileApp" target="_blank">app</a> here!`,
+    // "sofa-table": `A sofa. I like to relax here and eat food. I love Big Way Hotpot, so much I even created an app for them. Check out the <a href="https://github.com/PhilipZhangs/BigWayMobileApp" target="_blank">app</a> here!`,
+    "sofa-table": `im very passionate about fitness and living a healthy lifestyle, but i have an unhealthy relationship with food. i played varsity basketball all throughout highschool and still play in mens leagues. i also love hiking, snowboarding, climbing and running.`,
 
-    tv: `Here is my <a href="https://github.com/philipzhangs" target="_blank">Github</a>!`,
+    // tv: `Here is my <a href="https://github.com/philipzhangs" target="_blank">Github</a>!`,
+    tv: `here is my <a href="https://github.com/philipzhangs" target="_blank">github</a>. there's not much activity lately because of work.`,
 
-    bed: `Check out my <a href="https://www.linkedin.com/in/philipzhangs/" target="_blank">LinkedIn</a>!`,
+    // bed: `Check out my <a href="https://www.linkedin.com/in/philipzhangs/" target="_blank">LinkedIn</a>!`,
+    bed: `check out my <a href="https://www.linkedin.com/in/philipzhangs/" target="_blank">linkedIn</a>, if you ever want chat over some coffee, my dms are always open`,
 
-    "resume": `You found my <a href="https://github.com/PhilipZhangs/Resume" target="_blank">resume</a>!
+    // "resume": `You found my <a href="https://github.com/PhilipZhangs/Resume" target="_blank">resume</a>!
+    // <p></p>
+    // <div style="display: flex; justify-content: center; align-items: center; width: 100%; height: 540px; overflow: hidden;">
+    //   <img src="philipzhang_cv2024.png" style="max-width: 100%; max-height: 100%; object-fit: contain;">
+    // </div>`,
+    "resume": `this is my <a href="https://github.com/PhilipZhangs/Resume" target="_blank">resume</a>!
     <p></p>
     <div style="display: flex; justify-content: center; align-items: center; width: 100%; height: 540px; overflow: hidden;">
-      <img src="philipzhang_cv2024.png" style="max-width: 100%; max-height: 100%; object-fit: contain;">
+      <img src="philipzhang_cv2025.png" style="max-width: 100%; max-height: 100%; object-fit: contain;">
     </div>`,
 
-    projects: `My friends and I made this cool <a href="https://sites.google.com/view/cmpt362-cell-identifier-g24/home?authuser=0" target="_blank">study tool</a> for biology students! We developed the mobile app using Android Studio and Kotlin.`,
+    projects: `some peers and i made this <a href="https://sites.google.com/view/cmpt362-cell-identifier-g24/home?authuser=0" target="_blank">study tool</a> for biology students. we developed the mobile app using android studio and kotlin.`,
 
-    library: `Under construction`,
+    library: `some photos just to give you an idea of the kind of person i am
+    <p></p>
+    <div style="display: flex; justify-content: center; align-items: center; width: 100%; height: 540px; overflow: hidden;">
+      <img src="hike-back.jpeg" style="max-width: 100%; max-height: 100%; object-fit: contain;">
+    </div>`,
 
     furniture: `Under construction`,
 
-    exit: `Thanks for checking out my portfolio! To exit, just close the tab.`,
+    exit: `thanks for checking out my portfolio. to exit, just close the tab.`,
   };
