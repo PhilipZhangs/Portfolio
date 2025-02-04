f5411ef1024370a18086c4c9bfec0e95429a967a
Phil <zhangsphilip@gmail.com>
2025-02-04 01:38:36 -0800

    updated spelling errors

--- src/constants.js
+++ src/constants.js
@@ -1,26 +1,16 @@
 export const scaleFactor = 4;
 
 export const dialogueData = {
-    // pc: `This is my computer. In my personal life, I enjoy gaming with friends. Growing up I loved playing Maplestory but now I mainly play titles from Riot Games with friends.`,
     pc: `in my spare time, i like to unwind by playing video games with friends. i rarely play solo because i only do it to spend time with others. some titles i play are League of Legends, Valorant, Marvel Rivals, Stardew Valley and Minecraft.`,
 
-    // "cs-degree": `This is my CS degree. I hung it on the wall because I'm proud of it. I started my undergrad studying Business, but during the COVID-19 pandemic, I discovered a passion for technology and here I am now!`,
     "cs-degree": `i studied computer science and graduated from sfu. im very proud of this accomplishment because for half of my undergrad, i couldn't figure out what i wanted to do. i started in econ, switched into business and then found myself in comp sci in the midst of covid. fun fact; i had to retake data structures and algorithms twice, its a little embarrassing but humility builds character right?`,
 
-    // "sofa-table": `A sofa. I like to relax here and eat food. I love Big Way Hotpot, so much I even created an app for them. Check out the <a href="https://github.com/PhilipZhangs/BigWayMobileApp" target="_blank">app</a> here!`,
     "sofa-table": `im very passionate about fitness and living a healthy lifestyle, but i have an unhealthy relationship with food. i played varsity basketball all throughout highschool and still play in mens leagues. i also love hiking, snowboarding, climbing and running.`,
 
-    // tv: `Here is my <a href="https://github.com/philipzhangs" target="_blank">Github</a>!`,
     tv: `here is my <a href="https://github.com/philipzhangs" target="_blank">github</a>. there's not much activity lately because of work.`,
 
-    // bed: `Check out my <a href="https://www.linkedin.com/in/philipzhangs/" target="_blank">LinkedIn</a>!`,
-    bed: `check out my <a href="https://www.linkedin.com/in/philipzhangs/" target="_blank">linkedIn</a>, if you ever want chat over some coffee, my dms are always open`,
+    bed: `check out my <a href="https://www.linkedin.com/in/philipzhangs/" target="_blank">linkedin</a>, if you ever want chat over some coffee, my dms are always open`,
 
-    // "resume": `You found my <a href="https://github.com/PhilipZhangs/Resume" target="_blank">resume</a>!
-    // <p></p>
-    // <div style="display: flex; justify-content: center; align-items: center; width: 100%; height: 540px; overflow: hidden;">
-    //   <img src="philipzhang_cv2024.png" style="max-width: 100%; max-height: 100%; object-fit: contain;">
-    // </div>`,
     "resume": `this is my <a href="https://github.com/PhilipZhangs/Resume" target="_blank">resume</a>!
     <p></p>
     <div style="display: flex; justify-content: center; align-items: center; width: 100%; height: 540px; overflow: hidden;">
@@ -31,19 +21,22 @@ export const dialogueData = {
 
     library: `some photos just to give you an idea of the kind of person i am
     <p></p>
+    <div style="display: flex; flex-direction: column; align-items: center; width: 100%; height: 540px; overflow-y: auto; scrollbar-width: thin; scrollbar-color: rgba(0, 0, 0, 0.5) transparent;">
+
     <div style="display: flex; justify-content: center; align-items: center; width: 100%; height: 540px; overflow: hidden;">
       <img src="hike-headshot.jpeg" style="max-width: 100%; max-height: 100%; object-fit: contain;">
-    </div>
-    
-    <div style="display: flex; justify-content: center; align-items: center; width: 100%; height: 540px; overflow: hidden;">
-      <img src="basketball.JPG" style="max-width: 100%; max-height: 100%; object-fit: contain;">
     </div>
 
     <div style="display: flex; justify-content: center; align-items: center; width: 100%; height: 540px; overflow: hidden;">
       <img src="sf.jpeg" style="max-width: 100%; max-height: 100%; object-fit: contain;">
     </div>
-    `
-    ,
+
+    <div style="display: flex; justify-content: center; align-items: center; width: 100%; height: 540px; overflow: hidden;">
+      <img src="basketball.JPG" style="max-width: 100%; max-height: 100%; object-fit: contain;">
+    </div>
+
+    </div>
+    `,
 
     furniture: `under construction until i think of something clever`,
 
